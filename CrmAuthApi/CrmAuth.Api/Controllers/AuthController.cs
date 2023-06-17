aa81e47d7eb2548cdcf36d994774c9078541cb55
Ed2002 <ed49516@gmail.com>
2023-06-17 01:31:35 -0300

    NugetPackges and Auth Controller

---
+++ CrmAuthApi/CrmAuth.Api/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrmAuth.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpGet("Ok")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
